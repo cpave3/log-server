ffaa5d0d154c0f8631b0b60dce0a8a914b97c9ec
Cameron Pavey <cpave3@gmail.com>
2018-07-21 20:32:10 +1000

    :construction: Added authorisation policy

---
+++ api/policies/isAuthorised.js
@@ -0,0 +1,38 @@
+module.exports = (req, res, next) => {
+    let token;
+
+    // Check if Auth header is present
+    if (req.headers && req.headers.authorization) {
+        // Header is present
+        const parts = req.headers.authorization.split(' ');
+        if (parts.length == 2) {
+            const scheme = parts[0];
+            const credentials = parts[1];
+
+            if(/^Bearer$/i.test(scheme)) {
+                token = credentials;
+            }
+        } else {
+            return res.status(401).json({
+                err: 'Format is Authorization: Bearer [token]'
+            });
+        }
+    } else {
+        // Authorization header is not present
+        return res.status(401).json({
+            err: 'No Authorization token provided'
+        });
+    }
+    
+    JwtService.verify(token, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                err: 'Invalid token provided'
+            });
+        }
+
+        req.user = decoded;
+        next();
+    });
+    
+}
