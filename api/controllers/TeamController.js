ffc58bd6e73823e851be987296d0a6d6ae9a2014
Cameron Pavey <cpave3@gmail.com>
2018-07-26 18:45:15 +1000

    :construction: Added WIP on TeamController.create

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -12,7 +12,48 @@ module.exports = {
         return res.status(200).json({controller: 'team'});
     },
 
-    create: (req, res) => {},
+    /**
+     * This method will allow for the creation of a team.
+     * Any user may create a team currently, and the creator 
+     * will be granted ownership of the team
+     */
+    create: (req, res) => {
+        try {
+            // Verify that the required inputs have been provided
+            const requiredFields = [
+                'name'
+            ];
+        
+            requiredFields.forEach(field => {
+                // We need to validate that each field marked as required is present
+                // Remove the found item from the requireds array
+                if (req.body[field]) {
+                    requiredFields.splice(requiredFields.indexOf(field), 1);
+                }
+
+                // If we have anything left in the array, it is missing from the request
+                if (requiredFields.length > 0) {
+                    // This suggests that a field is missing
+                    const status = 400;
+                    const errorData = {
+                        missingField: [...requiredFields]
+                    };
+                    throw new Error('Request is missing required fields.');
+                }
+
+                // If we made it this far, we should have enough data to make a new Team
+            });
+            
+        } catch (error) {
+            return res
+            .status(status || 500)
+            .json({
+                error: true,
+                message: error.message,
+                data: errorData || {}
+            });
+        }
+    },
 
     read: (req, res) => {},
 
